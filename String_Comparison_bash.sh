4a5df0fa6a9d2e524be30651ea2c13df3b9177f9
kanwar2preet <kanwar2preet@gmail.com>
2016-03-31 18:13:46 -0700

    Create String_Comparison_bash.sh

---
+++ String_Comparison_bash.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+# Purpose : Comparing two strings in bash
+
+if [ "practise__bash" = "practisebash"  ]
+then
+	echo both values are equal
+else
+	echo Both are unequal
+
+fi
+
+
+#END
