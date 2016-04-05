c80a526561f2e7aa8e883b5a5ba79f58801c4be3
kanwar2preet <kanwar2preet@gmail.com>
2016-04-05 15:04:48 -0700

    Update ssh_key_add_multiple_servers.sh

--- ssh_key_add_multiple_servers.sh
+++ ssh_key_add_multiple_servers.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-# Adding ssh-key in multiple servers
+# Purpose : Adding ssh-key in multiple servers
+# Downside is we have to enter passwords each time
 
 for ip in `cat /Users/kansingh/Scripts/poolx`; do
     ssh-copy-id  $ip
 done
+
+
+#END
